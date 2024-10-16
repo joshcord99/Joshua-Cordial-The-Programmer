c0ce815bcb32637c4dd5d1c1c18d4af694f02bdb
joshcord99 <joshcord99@gmail.com>
2024-10-16 16:03:07 -0700

    seperated pages into files

---
+++ Joshua-Cordial-The-Programmer/src/pages/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const aboutMe = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the About Me Page</p>
+    </div>
+  );
+};
+
+export default aboutMe;
---
+++ Joshua-Cordial-The-Programmer/src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const errorPage = () => {
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <p>This is the Error Page</p>
+    </div>
+  );
+};
+
+export default errorPage;
