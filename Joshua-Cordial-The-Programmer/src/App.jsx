a63205adc3712440def014e4896bd47f47527b67
joshcord99 <joshcord99@gmail.com>
2024-10-15 17:06:51 -0700

    layed out images and div tags

--- Joshua-Cordial-The-Programmer/src/App.jsx
+++ Joshua-Cordial-The-Programmer/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +7,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/joshcord99" target="_blank">
+        <img src="/AvatarMaker-2.png" className="avatarImage" alt="Avatar"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Joshua Cordial</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi alias sit eos illo ratione quia culpa ut, architecto repudiandae pariatur incidunt, aperiam ducimus, aut esse dicta perferendis! Labore, laboriosam omnis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati praesentium cum et! Corporis incidunt ea commodi ipsam saepe excepturi nisi voluptate quae, explicabo reiciendis nam. Ipsa adipisci sed illo distinctio
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quos non aliquid velit facere necessitatibus laborum expedita, eius eligendi alias laboriosam provident nesciunt ipsum fuga? Voluptatem hic est velit delectus!
+      </footer>
     </>
   )
 }
