3de7773efe0349951ac8bfe33a539db338222306
joshcord99 <joshcord99@gmail.com>
2024-10-16 16:03:07 -0700

    seperated pages into files

--- Joshua-Cordial-The-Programmer/src/App.jsx
+++ Joshua-Cordial-The-Programmer/src/App.jsx
@@ -1,16 +1,31 @@
+// // Bringing in the required import from 'react-router-dom'
+// import { Outlet } from 'react-router-dom';
+// import Nav from './components/Nav';
+
+// function App() {
+//   // The Outlet component will conditionally swap between the different pages according to the URL
+//   return (
+//     <>
+//       <Nav />
+//       <Outlet />
+
+//     </>
+//   );
+// }
+
+// export default App;
 import { useState } from 'react';
 import './App.css';
 
+
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
         <nav className="navbar">
           <ul className="navbar-nav">
-
-          <li className="nav-item name">
+            <li className="nav-item name">
               <a href="#name" className="nav-link name">Joshua Cordial</a>
             </li>
             <li className="nav-item">
@@ -19,6 +34,7 @@ function App() {
             <li className="nav-item">
               <a href="#portfolio" className="nav-link">Portfolio</a>
             </li>
+
             <li className="nav-item">
               <a href="#contact" className="nav-link">Contact</a>
             </li>
@@ -28,13 +44,10 @@ function App() {
           </ul>
         </nav>
       </div>
-      {/* <div>
-      <h1 className='name'>Joshua Cordial</h1>
-      </div> */}
       <div><h2 className='topic'>About Me</h2></div>
       <div>
         <a href="https://github.com/joshcord99" target="_blank">
-          <img src="/AvatarMaker-2.png" className="avatarImage" alt="Avatar"/>
+          <img src="/AvatarMaker-2.png" className="avatarImage" alt="Avatar" />
         </a>
       </div>
       <div className="card">
@@ -42,12 +55,11 @@ function App() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi alias sit eos illo ratione quia culpa ut, architecto repudiandae pariatur incidunt, aperiam ducimus, aut esse dicta perferendis! Labore, laboriosam omnis.
         </p>
         <div className="card">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati praesentium cum et! Corporis incidunt ea commodi ipsam saepe excepturi nisi voluptate quae, explicabo reiciendis nam. Ipsa adipisci sed illo distinctio
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati praesentium cum et! Corporis incidunt ea commodi ipsam saepe excepturi nisi voluptate quae, explicabo reiciendis nam. Ipsa adipisci sed illo distinctio
+          </p>
         </div>
       </div>
-
       <footer>
         This is the footer
       </footer>
--- Joshua-Cordial-The-Programmer/src/main.jsx
+++ Joshua-Cordial-The-Programmer/src/main.jsx
@@ -8,3 +8,42 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+import App from './App';
+import ErrorPage from './pages/ErrorPage.jsx';
+// import AboutMe from './pages/AboutMe.jsx';
+import PortfolioPage from './pages/PortfolioPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage.jsx'
+import ResumePage from './pages/ResumePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'aboutme',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
