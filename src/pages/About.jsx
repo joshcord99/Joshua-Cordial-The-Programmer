5ec298f65f8c7ebc3a7bc4de8b0f2c4ada06b85f
joshcord99 <joshcord99@gmail.com>
2024-11-11 22:56:16 -0800

    edited summary

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,14 +3,14 @@ import "../css/About.css";
 export default function About() {
   return (
     <div className="text-center">
-      <header className='aboutHead'>Joshua Cordial</header>
+      <header className="aboutHead">Joshua Cordial</header>
       <div>
         <a href="https://github.com/joshcord99" target="_blank">
           <img src="/me.jpg" className="avatarImage" alt="Avatar" />
         </a>
       </div>
       <p className="aboutText">
-        I am a 25 year old , entry-level software devloper based in California I am a motivated and adaptable software developer with experience in full-stack development, specializing in building web applications using modern frameworks. Proven ability to learn new skills quickly and contribute to team success. Strong work ethic, problem-solving abilities, and eﬀective communication skills. Eager to leverage my software development expertise and bring positive, results-driven attitude to my employer.
+      I am a 25-year-old entry-level software developer based in California. I am motivated and adaptable, with experience in full-stack development, specializing in building web applications using modern frameworks. I have a proven ability to learn new skills quickly and contribute to team success. I possess a strong work ethic, excellent problem-solving abilities, and effective communication skills. I am eager to leverage my software development expertise and bring a positive, results-driven attitude to my employer.
       </p>
     </div>
   );
