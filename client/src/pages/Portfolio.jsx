6b3ddaac03dacba564d2efef642199fafdb54b60
Joshua Cordial <joshuacordial@Mac.lan>
2024-11-29 23:19:39 -0800

    added to projects

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -28,11 +28,7 @@ export default function Portfolio() {
             href="https://github.com/joshcord99/Personal-Vehicle-Builder-Generator"
             target="_blank"
           >
-            <img
-              src="/VehicleBuilder.jpg"
-              className="image"
-              alt="Project2"
-            />
+            <img src="/VehicleBuilder.jpg" className="image" alt="Project2" />
           </a>
 
           <p className="portfolioText">Personal-Vehicle-Builder-Generator</p>
@@ -42,39 +38,45 @@ export default function Portfolio() {
             href="https://github.com/joshcord99/Efficient-Studying"
             target="_blank"
           >
-            <img
-              src="/shortcutlogo.jpeg"
-              className="image"
-              alt="Project4"
-            />
+            <img src="/shortcutlogo.jpeg" className="image" alt="Project4" />
           </a>
           <p className="portfolioText">Efficient-Studying</p>
         </div>
+
         <div className="card">
           <a
             href="https://github.com/joshcord99/Personal-Weather-Information"
             target="_blank"
           >
-            <img
-              src="/weatherimage.jpg"
-              className="image"
-              alt="Project5"
-            />
+            <img src="/weatherimage.jpg" className="image" alt="Project5" />
           </a>
           <p className="portfolioText">Personal-Weather-Information</p>
         </div>
+
         <div className="card">
           <a
             href="https://github.com/joshcord99/Joshua-Cordial-The-Programmer"
             target="_blank"
           >
-            <img
-              src="/AvatarMaker-2.png"
-              className="image"
-              alt="Project6"
-            />
+            <img src="/AvatarMaker-2.png" className="image" alt="Project6" />
           </a>
           <p className="portfolioText">Joshua-Cordial-The-Programmer</p>
+        </div>
+
+        <div className="card">
+          <a href="https://github.com/anthonymoon2/TomoPudgy" target="_blank">
+            <img src="/tamapudgy.png" className="image" alt="Project7" />
+          </a>
+          <p className="portfolioText">
+            TamaPudgy | Collaborator on Back-End Development
+          </p>
+        </div>
+
+        <div className="card">
+          <a href="https://github.com/joshcord99/Talker" target="_blank">
+            <img src="/zen.jpg" className="image" alt="Project8" />
+          </a>
+          <p className="portfolioText">Talker - AI Therapist</p>
         </div>
       </section>
     </div>
